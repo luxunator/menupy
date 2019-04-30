dcd1f816cee2c6fd79368e14cf64cd3f79bc0209
luxunator <dsdilpickle0519@gmail.com>
2019-04-30 15:10:05 -0600

    Setup Created

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name="menupy",
+	version="1.0.0",
+	author="luxunator",
+	author_email="luxunator@pm.me",
+	url="https://github.com/luxunator/menupy",
+	description="Interactive Python Menu",
+	keywords='menu menupy curses',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	license='MIT',
+	classifiers=[
+		'Development Status :: 5 - Production/Stable',
+		'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+	)
